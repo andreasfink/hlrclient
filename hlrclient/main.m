e42a55463e56c8f79b164a84b763a3e40bf7f7b8
Andreas Fink <andreas@fink.org>
2017-05-10 07:17:42 +0200

    Initial Commit

---
+++ hlrclient/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hlrclient
+//
+//  Created by Andreas Fink on 10.05.17.
+//  Copyright © 2017 Andreas Fink. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
